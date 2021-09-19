7ce001181d91cb949449b67b8dc3a99370a520df
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-09-19 14:16:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ read -p "> " pil
 if [ $pil = "1" ]
 then
     sudo apt install python3
-    sudo python3 install-for-aplication.py
+    sudo python3 install-with-application.py
 elif [ $pil = "2" ]
 then
     sudo apt install python3
-    sudo python3 install.py
+    sudo python3 install-no-application.py
 
 fi

914765ff28d97daba349d91dbc340ac29bc7c00f
Alanlol <81538835+alanlol12@users.noreply.github.com>
2021-09-21 09:30:52 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,7 @@
+echo update...
+sleep 4
+git clone https://github.com/alanlol12/nisefuca
+cd nisefuca
+sudo rm -rf /opt/nisefuca
+chmod +x *
+./install.sh
---
+++ updatenis.sh
@@ -0,0 +1,1 @@
+bash /opt/nisefuca/update/update.sh
